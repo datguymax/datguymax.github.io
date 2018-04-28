9b176ec446e5298b6f8f2aa0aa5e0569cf7cd895
Californ1a <rk_california@yahoo.com>
2018-04-28 03:17:35 -0400

    cleanup and stylistic changes with added code comments

--- js/scripts.js
+++ js/scripts.js
@@ -24,24 +24,42 @@ function nemi(level, cap) {
 }
 
 function calculateDays() {
-
-	const tbIds = ["tb-current-prayer-xp", "tb-current-farming-xp", "tb-current-mining-xp", "tb-current-dungeoneering-xp"];
-	const tarTypes = ["dd-prayer-target-type", "dd-farming-target-type", "dd-mining-target-type", "dd-dungeoneering-target-type"];
-	const tarGoals = ["tb-prayer-goal", "tb-farming-goal", "tb-mining-goal", "tb-dungeoneering-goal"];
-	const spnIds = ["spn-days-prayer", "spn-days-farming", "spn-days-mining", "spn-days-dungeoneering"];
+	const tbIds = [
+		document.getElementById("tb-current-prayer-xp").value,
+		document.getElementById("tb-current-farming-xp").value,
+		document.getElementById("tb-current-mining-xp").value,
+		document.getElementById("tb-current-dungeoneering-xp").value
+	];
+	const tarTypes = [
+		document.getElementById("dd-prayer-target-type").value,
+		document.getElementById("dd-farming-target-type").value,
+		document.getElementById("dd-mining-target-type").value,
+		document.getElementById("dd-dungeoneering-target-type").value
+	];
+	const tarGoals = [
+		document.getElementById("tb-prayer-goal").value,
+		document.getElementById("tb-farming-goal").value,
+		document.getElementById("tb-mining-goal").value,
+		document.getElementById("tb-dungeoneering-goal").value
+	];
+	const spnIds = [
+		document.getElementById("spn-days-prayer"),
+		document.getElementById("spn-days-farming"),
+		document.getElementById("spn-days-mining"),
+		document.getElementById("spn-days-dungeoneering")
+	];
 	const nodes = [4, 3, 1, 1];
 	const cap = [99, 99, 99, 120];
 
 	for (let i = 0; i < tbIds.length; i++) {
-		const targetType = document.getElementById(tarTypes[i]).value;
-		let currentXP = parseFloat(document.getElementById(tbIds[i]).value);
+		let currentXP = parseFloat(tbIds[i]);
 		let goal;
 		let days = 0;
 
-		if (targetType === "xp") {
-			goal = parseFloat(document.getElementById(tarGoals[i]).value);
-		} else if (targetType === "level") {
-			goal = parseFloat(document.getElementById(tarGoals[i]).value).toXP();
+		if (tarTypes[i] === "xp") {
+			goal = parseFloat(tarGoals[i]);
+		} else if (tarTypes[i] === "level") {
+			goal = parseFloat(tarGoals[i]).toXP();
 		}
 
 		while (currentXP < goal) {
@@ -51,21 +69,30 @@ function calculateDays() {
 			days++;
 		}
 
-		document.getElementById(spnIds[i]).textContent = days.toLocaleString();
+		spnIds[i].textContent = days.toLocaleString();
 	}
 }
 
 function getStats() {
 	const tbIds = ["tb-current-prayer-xp", "tb-current-farming-xp", "tb-current-mining-xp", "tb-current-dungeoneering-xp"];
 	const username = document.getElementById("tb-username").value;
-	if (username === "") {
+	if (username === "") { //don't try to fetch empty string
 		return;
 	}
-	fetch(`https://cors-anywhere.herokuapp.com/http://services.runescape.com/m=hiscore/index_lite.ws?player=${username}`)
-		.then(res => res.text())
-		.then(data => data.split(/[\s\r\n,]+/))
+	fetch(`https://cors-anywhere.herokuapp.com/http://services.runescape.com/m=hiscore/index_lite.ws?player=${username}`) //use cors proxy because of Access-Control-Allow-Origin
+		.then(res => res.text()) //convert response object to text data
+		.then(data => data.split(/[\s\r\n,]+/)) //split csv by spaces, newlines, and commas into an array
 		.then(data => {
-			const skills = [parseInt(data[20]), parseInt(data[62]), parseInt(data[47]), parseInt(data[77])];
+			//indicies in hiscore csv for each skill
+			const prayIndex = 20;
+			const farmIndex = 62;
+			const mineIndex = 47;
+			const dgIndex = 77;
+
+			//put in ordered array
+			const skills = [data[prayIndex], data[farmIndex], data[mineIndex], data[dgIndex]];
+
+			//set value in each xp input
 			for (let i = 0; i < skills.length; i++) {
 				document.getElementById(tbIds[i]).value = skills[i];
 			}
